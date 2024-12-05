d876f4f2e0d195108f325c2c72a65bc9db3bc59b
Andre Zicatti <andrezicatti@gmail.com>
2024-12-05 16:55:55 -0300

    Create and test database connection.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            System.out.println("Conexão com banco de dados estabelecida!");
+        } catch (Exception e) {
+            System.err.println("Erro ao conectar: " + e.getMessage());
+        }
     }
 }
